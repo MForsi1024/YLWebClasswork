89324afa452cec2d362c934fc89bf3bc7efb846d
Karakozov Matvey <matveykax@yandex.ru>
2026-03-11 19:16:32 +0400

    First

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, url_for, request, render_template, redirect
+
+from data.login_form import LoginForm
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key!!!'
+@app.route('/')
+@app.route('/index/')
+def index():
+    return render_template('index.html', title='Подро дожаловать!')
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof, title="Тренировки")
+
+
+@app.route("/list_prof/<type>")
+def list_prof(type):
+    profs = ['Инженер', "Врач", "Метеоролог", "Инженер-строитель", "Штурман", "Иженер-исследователь"]
+    return render_template("list_prof.html", type=type, profs=profs)
+
+
+@app.route("/answer")
+@app.route("/auto_answer")
+def answer():
+    param = {}
+    param["title"] = "Анкета"
+    param["surname"] = "Иванов"
+    param["name"] = "Иван"
+    return render_template('answer.html', **param)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/answer')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
