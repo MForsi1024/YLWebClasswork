95786f46fb430233038f23ab8343c478c94281fb
Karakozov Matvey <matveykax@yandex.ru>
2026-03-11 19:18:36 +0400

    Second

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, url_for, request, render_template, redirect
-
 from data.login_form import LoginForm
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key!!!'
+
+
 @app.route('/')
 @app.route('/index/')
 def index():
